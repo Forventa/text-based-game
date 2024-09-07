a4b03933edee5abcaee5d75c4a868aa510cd6f5e
Forventa <ttbloons@gmail.com>
2024-09-07 12:59:07 +0200

    added simple api and server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+
+app.use("/employees", require("./routes/api/employees"));
+
+
+app.get("/", (req, res) =>{
+    res.sendFile("./views/index.html", {root: __dirname});
+});
+
+
+app.get("/login.html", (req, res) =>{
+    res.sendFile(path.join(__dirname, 'views', "login.html"));
+});
+
+app.listen(port, () => console.log(`server is listening on port ${port}`));
